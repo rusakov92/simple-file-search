044f170209d0ee83e5c488c1821c76feee1c3e63
Aleksandar Rusakov <aleksandar.rusakov@rewardgateway.com>
2019-04-01 15:25:57 +0300

    Add a UI demo

--- symfony_demo/src/AppBundle/Controller/DefaultController.php
+++ symfony_demo/src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use App\SimpleFileSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,8 +19,6 @@ class DefaultController extends Controller
      */
     public function index(Request $request) : Response
     {
-//        dump($request->request->all());die;
-
         return $this->render('default/index.html.twig');
     }
 
@@ -33,6 +31,30 @@ class DefaultController extends Controller
      */
     public function search(Request $request) : Response
     {
-        dump($request->request->all());die;
+        $contains = $request->request->get('contains');
+        if (empty($contains)) {
+            return $this->render('default/index.html.twig', [
+                'error' => 'Please provide some content to search for.',
+            ]);
+        }
+        $contains = \explode(',', \htmlspecialchars($contains));
+        \array_walk($contains, function (&$content) {
+            $content = \trim($content);
+            if (\preg_match('{^#.+#[imsxADSUXJu]*$}', $content) !== 1) {
+                $content = \sprintf('#%s#', $content);
+            }
+        });
+
+        $simpleFileSearch = new SimpleFileSearch(__DIR__.'/../../../../public/demo_files');
+
+        $result = $simpleFileSearch->contains($contains)->find();
+        $result = \iterator_to_array($result);
+        if (empty($result)) {
+            return $this->render('default/index.html.twig', [
+                'error' => 'No files found!',
+            ]);
+        }
+
+        return $this->render('default/index.html.twig', ['files' => $result]);
     }
 }
